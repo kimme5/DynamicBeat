cacddadc73b242857e2e4d775ef039179cd23cfb
kimme5 <seongwoo711@gmail.com>
2023-02-09 17:49:50 +0900

    Dynamic Beat #16

---
+++ src/dynamic_beat_16/KeyListener.java
@@ -0,0 +1,68 @@
+package dynamic_beat_16;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyListener extends KeyAdapter {
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// Game이 진행중이 아니라면 아래의 Key Event 처리를 진행하지 않음
+		if(DynamicBeat.game == null) {
+			return;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_S) {
+			DynamicBeat.game.pressS();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_D) {
+			DynamicBeat.game.pressD();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_F) {
+			DynamicBeat.game.pressF();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+			DynamicBeat.game.pressSpace();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_J) {
+			DynamicBeat.game.pressJ();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_K) {
+			DynamicBeat.game.pressK();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_L) {
+			DynamicBeat.game.pressL();
+		}
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// Game이 진행중이 아니라면 아래의 Key Event 처리를 진행하지 않음
+		if(DynamicBeat.game == null) {
+			return;
+		}
+
+		if(e.getKeyCode() == KeyEvent.VK_S) {
+			DynamicBeat.game.releaseS();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_D) {
+			DynamicBeat.game.releaseD();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_F) {
+			DynamicBeat.game.releaseF();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+			DynamicBeat.game.releaseSpace();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_J) {
+			DynamicBeat.game.releaseJ();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_K) {
+			DynamicBeat.game.releaseK();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_L) {
+			DynamicBeat.game.releaseL();
+		}
+	}
+
+}
